e526ff7322866f69592b8a01905c1644b2e9ab76
Harrison Louis Greeves <hgreeves@protonmail.com>
2020-01-20 16:27:16 +0800

    Braintree client side integration has been updated.

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,11 @@
 <?php
 
-Route::view('/home', 'landing')->name('landing'); 
+Route::view('/', 'landing')->name('landing'); 
 Route::view('/checkout', 'checkout')->name('checkout');
 Route::view('/confirmation', 'confirmation')->name('confirmation');
 
 // enables backend form validation
 Route::post('/checkout', 'CheckoutController@store')->name('checkout.store'); 
-
-// enables product retrieval
-Route::get('/', 'ProductController@index')->name('product.index');
 
 // places the product in the cart
 Route::post('/', 'CartController@store')->name('cart.store'); 
